1c5a821f9e7dbd4aa24708601fa84c75dbc2b5ea
Aaron-Cunningham <aaroncunningham222@gmail.com>
2022-09-29 14:50:20 +0100

    Added an argument to enlarge panda

--- walking_panda/cli.py
+++ walking_panda/cli.py
@@ -11,10 +11,15 @@ import argparse
 
 def cli():
     parser = argparse.ArgumentParser(prog="walking_panda")
-    parser.add_argument("--no-rotate",help="Suppress Rotation",
+    parser.add_argument("--no-rotate", help="Suppress Rotation",
                         action="store_true")
+    parser.add_argument("--scale", help="Enlarge panda",
+                        action="store_true")
+
 
     args = parser.parse_args()
 
     walking = panda.WalkingPanda(**vars(args))
     walking.run()
+
+
