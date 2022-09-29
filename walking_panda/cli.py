c0170e3ffc7e6afc1c117b2182dd48122624b15d
Aaron-Cunningham <aaroncunningham222@gmail.com>
2022-09-29 22:42:21 +0100

    Added argument to change panda colour to blue

--- walking_panda/cli.py
+++ walking_panda/cli.py
@@ -15,6 +15,9 @@ def cli():
                         action="store_true")
     parser.add_argument("--scale", type=float, default=1, help="Enlarge panda * original size > scale = 1")
 
+    parser.add_argument("--colour-blue", help="Makes the panda blue",
+                        action="store_true")
+
 
     args = parser.parse_args()
 
