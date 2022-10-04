3f581507c1cd5052c70781bfef8bcc9c06c5020d
Aaron-Cunningham <aaroncunningham222@gmail.com>
2022-10-04 23:16:32 +0100

    Added panda movement functionality with the arrow keys

--- walking_panda/panda.py
+++ walking_panda/panda.py
@@ -1,8 +1,20 @@
 from math import pi, sin, cos
-import random
 from direct.actor.Actor import Actor
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
+
+# Keymap functions default set to false
+keyMap = {
+    "up": False,
+    "down": False,
+    "left": False,
+    "right": False
+}
+
+
+# function to update key map
+def keyMapUpdate(key, state):
+    keyMap[key] = state
 
 
 class WalkingPanda(ShowBase):
@@ -20,6 +32,20 @@ class WalkingPanda(ShowBase):
         # Apply scale and position transforms on the model.
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, 0)
+
+        # Sets the keymap functions to true when pressed and false when unpressed
+        # Which updates keyMapUpdate
+        self.accept("arrow_left", keyMapUpdate, ["left", True])
+        self.accept("arrow_left-up", keyMapUpdate, ["left", False])
+
+        self.accept("arrow_right", keyMapUpdate, ["right", True])
+        self.accept("arrow_right-up", keyMapUpdate, ["right", False])
+
+        self.accept("arrow_down", keyMapUpdate, ["up", True])
+        self.accept("arrow_down-up", keyMapUpdate, ["up", False])
+
+        self.accept("arrow_up", keyMapUpdate, ["down", True])
+        self.accept("arrow_up-up", keyMapUpdate, ["down", False])
 
         # Uses scale argument to change size of panda
         if scale:
@@ -68,6 +94,27 @@ class WalkingPanda(ShowBase):
             '/Users/aaroncunningham/PycharmProjects/C2060506_csc1034_practical1_2022/sounds/Sneaky-Snitch.mp3')
         pandaMusic.setLoop(True)
         pandaMusic.play()
+
+        # Sets pandas movement speed
+        self.speed = 0.05
+        # Runs update function
+        self.taskMgr.add(self.updatePandaPos, "updatePandaPos")
+
+    # function to update panda position on keypress
+    def updatePandaPos(self, task):
+        pos = self.pandaActor.getPos()
+        # if statements to move panda on keypress
+        if keyMap["left"]:
+            pos.x -= self.speed
+        if keyMap['right']:
+            pos.x += self.speed
+        if keyMap['up']:
+            pos.y += self.speed
+        if keyMap['down']:
+            pos.y -= self.speed
+
+        self.pandaActor.setPos(pos)
+        return task.cont
 
     # Define a procedure to move the camera.
     def spinCameraTask(self, task):
